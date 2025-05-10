9e1cfc35dfa862f4d6616026dc543bec2ce1b657
Himanshu Chaudhary <hchahudhary2@wayfair.com>
2025-05-10 16:05:31 +0530

    Add handlebars dependency

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://blog.buildwithhimanshu.com/'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
